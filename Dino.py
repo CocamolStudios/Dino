1b5675ecde66b796d52ce92d0bbd6b97987da9d8
CocamolStudios <99811122+CocamolStudios@users.noreply.github.com>
2022-08-24 12:29:14 -0700

    Add files via upload

---
+++ Dino.py
@@ -0,0 +1,15 @@
+
+def NUM(int):
+    print(int + int - int)
+
+def BIN(int):
+    print(bin(int))
+
+def HEX(int):
+    print(hex(int))
+
+def OCT(int):
+    print(oct(int))
+
+def DC(int):
+    print(hex(int+int*2))

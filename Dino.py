40f07f8c64e8171d8342bcfdac6bebbb140b5fe9
CocamolStudios <99811122+CocamolStudios@users.noreply.github.com>
2022-12-07 02:23:51 -0800

    Update Dino.py

--- Dino.py
+++ Dino.py
@@ -1,3 +1,6 @@
+import math
+
+import numpy as np
 
 def NUM(int):
     print(int + int - int)
@@ -19,3 +22,17 @@ def GHOST(int):
 
 def RS(int):
     print(hex(int + int - int * 2 + int + int - int))
+
+def OL(n1, n2, n3, n4):
+    print(hex(math.sqrt(n1 * n2 * n3 * n4 - (n1 * n4))))
+    a = [n1, n2, n3, n4]
+    print('your numbers:', a)
+    print('your numbers in hex:', hex(a))
+
+def PL(row1, row2):
+    a = np.array([
+        [row1, row2, row1 * row2]
+    ])
+    print('your row1 in bin:', bin(row1))
+    print('your row2 in bin:', bin(row2))
+    print('your rows array:', a)
